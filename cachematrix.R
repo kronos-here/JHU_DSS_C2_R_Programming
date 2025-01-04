fd12f7366f709d3038036ba5fc79f4e6a9c3e545
kronos-here <sayanrayz1998@gmail.com>
2025-01-05 01:00:20 +0530

    Initial of the caching mechanism for matrix inversion in R as asked in week6 Task

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,48 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initialization of a null x and cache_mat
+  cache_mat <- NULL
+  
+  ## This is to Globally modify the matrix inverse
+  set <- function(y) {
+    x <<- y
+    cache_mat <<- NULL
+  }
+  
+  ## To return a prompt for the original matrix
+  get <- function() x
+  
+  ## This function saves the inverse in the cache
+  setinmx <- function(inv_mat) cache_mat <<- inv_mat
+  
+  ## To fetch the cache of the inverse matrix
+  getinmx <- function() cache_mat
+  list(set = set, get = get,
+       setinmx = setinmx,
+       getinmx = getinmx)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## 'x' being the list of the component functions
+  inv_mat <- x$getinmx()
+  
+  ## The base case of the inverse being in the cache
+  if(!is.null(inv_mat)) {
+    message("getting cached data")
+    return(inv_mat)
+  }
+  
+  ## Else we proceed to calculate the inverse
+  ## The matrix is extracted
+  mat <- x$get()
+  ## Calculating the inverse of 'x' using solve()
+  inv_mat <- solve(mat, ...)
+  ## Storing the Matrix inverse in the cache
+  x$setinmx(inv_mat)
+  inv_mat
+  ## Return a matrix that is the inverse of 'x'
 }
